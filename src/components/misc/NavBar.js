402de7c663cb71e1d88c93802ff243c8fdc2bb37
Fran Leston <fran@Frans-MacBook-Air.local>
2019-03-31 22:10:26 +0200

    feat/ worked on user login, registration, roles and design

--- src/components/misc/NavBar.js
+++ src/components/misc/NavBar.js
@@ -4,7 +4,6 @@ import { withAuthConsumer } from "../../contexts/AuthStore";
 import { authService } from "../../services";
 
 class NavBar extends Component {
-  
   handleLogout = () => {
     authService.logout().then(() => {
       this.props.onUserChanged({});
@@ -15,77 +14,74 @@ class NavBar extends Component {
   render() {
     const { user, isAuthenticated, isAdmin } = this.props;
 
-  return (
-    <div className="container">
-      <div className="columns is-centered">
-        <div className="column is-12">
-          <nav
-            className="navbar is-link"
-            role="navigation"
-            aria-label="main navigation">
-            <div className="navbar-brand">
-              <a className="navbar-item" href="/">
-                <img
-                  src="https://bulma.io/images/bulma-logo.png"
-                  width="112"
-                  height="28"
-                />
-              </a>
-            </div>
-
-            <div id="navbarBasicExample" className="navbar-menu">
-              <div className="navbar-start">
-                <a className="navbar-item">For Landlords</a>
-                {isAdmin() &&
-                
-                <a className="navbar-item"><NavLink className="nav-link" activeClassName="active" to="/admin">Admin Zone</NavLink></a>}
-
-                <a className="navbar-item">For Tenants</a>
-                
-                
+    return (
+      <div className="container">
+        <div className="columns is-centered">
+          <div className="column is-12">
+            <nav
+              className="navbar is-link"
+              role="navigation"
+              aria-label="main navigation"
+            >
+              <div className="navbar-brand">
+                <a className="navbar-item" href="/">
+                  <img
+                    src="https://bulma.io/images/bulma-logo.png"
+                    width="112"
+                    height="28"
+                  />
+                </a>
               </div>
 
-              <div className="navbar-end">
-                <div className="navbar-item">
-                  <div className="buttons">
-                  {!isAuthenticated() &&
-              <Fragment>
-                    <a className="button is-info is-inverted">
-                      <NavLink
-                        className="nav-link"
-                        activeClassName="active"
-                        to="/register"
-                      >
-                        Register
-                      </NavLink>
-                    </a>
-                    </Fragment>
-                  }
-                    {isAuthenticated() &&
-              <Fragment>
-                    <a className="button is-info is-inverted">
-                      <NavLink
-                        className="nav-link"
-                        activeClassName="active"
-                        to="/login"
-                      >
-                        Login
-                      </NavLink>
-                    </a>
-                    </Fragment>
-                    }
+              <div id="navbarBasicExample" className="navbar-menu">
+                <div className="navbar-start">
+                  <a className="navbar-item">For Landlords</a>
+
+                  <a className="navbar-item">{user.email}</a>
+
+                  <a className="navbar-item">For Tenants</a>
+                  <a className="navbar-item">{user.email}</a>
+                </div>
+
+                <div className="navbar-end">
+                  <div className="navbar-item">
+                    <div className="buttons">
+                      {!isAuthenticated() && (
+                        <Fragment>
+                          <a className="button is-info is-inverted">
+                            <NavLink
+                              className="nav-link"
+                              activeClassName="active"
+                              to="/register"
+                            >
+                              Register
+                            </NavLink>
+                          </a>
+                        </Fragment>
+                      )}
+                      {isAuthenticated() && (
+                        <Fragment>
+                          <a className="button is-info is-inverted">
+                            <NavLink
+                              className="nav-link"
+                              activeClassName="active"
+                              to="/login"
+                            >
+                              Login
+                            </NavLink>
+                          </a>
+                        </Fragment>
+                      )}
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </nav>
+            </nav>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
   }
 }
-
-
 
 export default withRouter(withAuthConsumer(NavBar));
